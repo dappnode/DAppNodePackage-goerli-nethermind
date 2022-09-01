5bea4d2f49291f99ad53e23eefb13dc96eed4084
Pablo <mendez4a@gmail.com>
2022-09-01 18:21:55 +0200

    Implement globalEnvs

---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER" in
+"prysm-prater.dnp.dappnode.eth")
+  echo "Using prysm-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/prysm/jwtsecret.hex"
+  ;;
+"lighthouse-prater.dnp.dappnode.eth")
+  echo "Using lighthouse-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/lighthouse/jwtsecret.hex"
+  ;;
+"teku-prater.dnp.dappnode.eth")
+  echo "Using teku-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/teku/jwtsecret.hex"
+  ;;
+"nimbus-prater.dnp.dappnode.eth")
+  echo "Using nimbus-prater.dnp.dappnode.eth"
+  JWT_PATH="/security/nimbus/jwtsecret.hex"
+  ;;
+*)
+  echo "Using default"
+  JWT_PATH="/security/default/jwtsecret.hex"
+  ;;
+esac
+
+# Print the jwt to the dappmanager
+JWT=$(cat $JWT_PATH)
+curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
+
+exec Nethermind.Runner --config goerli \
+  --JsonRpc.Enabled=true \
+  --JsonRpc.JwtSecretFile=${JWT_PATH} \
+  --Init.BaseDbPath=/data \
+  --Init.LogDirectory=/data/logs \
+  $EXTRA_OPTS
